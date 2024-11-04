cc8c774082cdea006c730e41ad0f1a13fee6f96c
Diego Zapata <diego.zapata@roiback.com>
2024-11-04 11:42:17 -0500

    ajustes

--- components/Banner2/index.tsx
+++ components/Banner2/index.tsx
@@ -5,8 +5,8 @@ import styles from "./Banner2.module.sass";
 const Banner2 = () => {
     return (
         <div className={styles.section}>
-            <div className={cn("container", styles.container)}>
-                <div className={styles.wrap} data-aos="zoom-in">
+            <div className={cn("container", styles.container)} data-aos="fade-up">
+                <div className={styles.wrap}>
                     <div className={cn("h2", styles.title)}>Transforming your ideas into digital success</div>
                     <div className={styles.info}>
                     We blend innovative technologies with cutting-edge design to create digital products that excel in both function and form. Discover how our expertise brings ideas to life.
